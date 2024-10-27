d3305ad59e6865f2637dab5ce1d201a9ef569cb6
datmicw <dat.micf@gmail.com>
2024-10-27 15:51:03 +0700

    add audio

---
+++ Assets/GameMusicManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GameMusicManager : MonoBehaviour
+{
+    public static GameMusicManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); 
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
